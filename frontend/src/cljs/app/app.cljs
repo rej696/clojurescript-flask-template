49892ed2df88a43a102d5b1bd222301ccda873d9
Rowan_Surface <55083730+rej696@users.noreply.github.com>
2021-03-23 10:23:36 +0000

    update reagent api calls

--- frontend/src/cljs/app/app.cljs
+++ frontend/src/cljs/app/app.cljs
@@ -6,20 +6,23 @@
 
 ;; View ---------
 
-; (def current-time (atom nil))
+(def time-api "http://localhost:5000/api/v1/time")
 
+(def current-time (atom nil))
 
-(defn get-time [endpoint current-time]
+(defn get-time [endpoint]
   (go (let [response (<! (http/get endpoint {:with-credentials? false}))]
-        (prn (:status response))
-        (prn (:time (:body response)))
+        (prn (str "GET " time-api " status: "(:status response)))
+        (prn (str "GET " time-api " response: " (:time (:body response))))
         (reset! current-time (:time (:body response))))))
 
 (defn component
   []
-  (let [current-time (atom nil)]
-    (get-time "http://localhost:5000/api/v1/time" current-time)
-    (prn current-time)
-    (fn []
-      [:h1
-       (str "The current time is " @current-time)])))
+  (get-time time-api)
+  (fn []
+    [:div
+     [:h1
+      (str "The current time is " @current-time)]
+     [:div
+      [:input.btn {:type "button" :value "Update Time!"
+                   :on-click (fn [] (get-time time-api))}]]]))
